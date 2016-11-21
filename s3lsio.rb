0faeb6da9a7a5fd475479071e6468dfc45bdce48
Chris Jones <cj@hanscj.com>
2016-11-21 16:59:31 -0500

    Removed version info

--- s3lsio.rb
+++ s3lsio.rb
@@ -2,7 +2,6 @@ class S3lsio < Formula
   desc "S3 command-line utility."
   homepage "http://www.lambdastack.io/s3lsio"
   url "https://s3.amazonaws.com/s3lsio/mac/s3lsio-0.1.16.tar.gz"
-  version "0.1.16"
   sha256 "686c2a548375d95afffe7e3860b7d9fa4e7d13e2df7544620f821cb2bc511edf"
 
   def install
