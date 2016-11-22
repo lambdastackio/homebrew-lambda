0ff43bdf0da4feab32afe31a7276f2be9dc0a82a
Chris Jones <cj@hanscj.com>
2016-11-22 10:58:50 -0500

    Changed directory

--- s3lsio.rb
+++ s3lsio.rb
@@ -1,7 +1,7 @@
 class S3lsio < Formula
   desc "S3 command-line utility."
   homepage "http://www.lambdastack.io/s3lsio"
-  url "https://s3.amazonaws.com/s3lsio/mac/s3lsio-0.1.17.tar.gz"
+  url "https://s3.amazonaws.com/s3lsio/osx/s3lsio-0.1.17.tar.gz"
   sha256 "dffd1f0129ad0478a56d7ecd47ac6bc9e6d26edc31ad06c524b6bc958f9b64f4"
 
   def install
