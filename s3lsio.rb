0a41d8c237a60997c12ab27913367738c17c2c98
Chris Jones <cj@hanscj.com>
2016-11-25 09:56:04 -0500

    Updated new version

--- s3lsio.rb
+++ s3lsio.rb
@@ -1,8 +1,8 @@
 class S3lsio < Formula
   desc "S3 command-line utility."
   homepage "http://www.lambdastack.io/s3lsio"
-  url "https://s3.amazonaws.com/s3lsio/osx/s3lsio-0.1.17.tar.gz"
-  sha256 "64f8bda05b1bd2144d74e798ab815205d396ad5f1e862a7e8b2ca91aded2ca7a"
+  url "https://s3.amazonaws.com/s3lsio/osx/s3lsio-0.1.18.tar.gz"
+  sha256 "d7da97fb52b0fa2b0dbf229b0290ca5b33735ca02b03cb3079f05960a2878bd0"
 
   def install
     bin.install "s3lsio"
